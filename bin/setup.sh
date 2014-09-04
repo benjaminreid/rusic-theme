ab298415c29a5115b896eea653c66fef7067f411
Benjamin Reid <benjamin@nouveller.com>
2014-09-04 10:30:21 +0100

    Add setup for bower and npm and then run the npm installs

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+bower init;
+npm init;
+
+npm install --save coffee-script gulp gulp-coffee gulp-concat gulp-minify-css gulp-ruby-sass gulp-svg2png gulp-uglify gulp-webserver main-bower-files;
